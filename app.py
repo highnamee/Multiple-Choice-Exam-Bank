fb85d185f6eafbab0c3baa164da6de4611825d68
anhnguyen6529 <anhnguyen6529@gmail.com>
2020-12-25 22:08:12 +0700

    Update app.py and folder templates

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
-
+from flask import Flask, render_template
+ 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/mcq')
+def mcq():
+    return render_template('mcq.html')
+
+@app.route('/exam')
+def exam():
+    return render_template('exam.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
